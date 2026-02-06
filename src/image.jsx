40dcef5bb87386e63f6cdd80bd96cd0e3e5487c6
Jessica Kiroff <jesskiroff@gmail.com>
2026-02-06 13:57:53 -0500

    add images

---
+++ src/image.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './image.css';
+
+function Image() {
+    return(
+        <image>
+            <img src = "https://bneirachel.com/wp-content/uploads/2020/07/DJI_0007-7-1024x682.jpg"></img>
+            <img src = "https://www.israelhayom.com/wp-content/uploads/2021/09/bnei-rachel-compound.png"></img>
+        </image>
+    )
+}
+
+export default Image;  
