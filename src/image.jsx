c3a3f712786eb37396289618835f543d567cb537
Jessica Kiroff <jesskiroff@gmail.com>
2026-02-11 11:56:49 -0500

    fix image css

--- src/image.jsx
+++ src/image.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import './image.css';
+import './Image.css';
 
 function Image() {
     return(
-        <image>
-            <img src = "https://bneirachel.com/wp-content/uploads/2020/07/DJI_0007-7-1024x682.jpg"></img>
-            <img src = "https://www.israelhayom.com/wp-content/uploads/2021/09/bnei-rachel-compound.png"></img>
-        </image>
+        <div>
+            
+            <img className= "bneiRachelImage" 
+                 src= "https://bneirachel.com/wp-content/uploads/2020/07/DJI_0007-7-1024x682.jpg"
+                 alt = "image" >
+            </img>
+
+
+            <img className= "bneiRachelImage" 
+                  src = "https://www.israelhayom.com/wp-content/uploads/2021/09/bnei-rachel-compound.png"
+                  alt = "image" >
+             </img>
+
+        </div>
     )
 }
 
