547655b3b841f798a66da3672a77fcb293926cbc
Jessica Kiroff <jesskiroff@gmail.com>
2026-05-19 15:41:28 -0400

    change contact-success css

--- src/pages/ContactSuccess.jsx
+++ src/pages/ContactSuccess.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useLanguage } from "../LanguageContext";
 import translations from "../translations";
+import backgroundImg from "../assets/bryWallLogo.jpeg";
 import "./ContactSuccess.css";
 
 function ContactSuccess() {
@@ -8,9 +9,12 @@ function ContactSuccess() {
   const t = translations[language];
 
   return (
-    <div className="contact-success-page">
-      <div className="contact-success-card">
-        <div className="contact-success-icon">✉️</div>
+    <div
+      className="contact-success-page"
+      style={{ backgroundImage: `url(${backgroundImg})` }}
+    >
+      <div className="contact-success-content">
+        <div className="contact-success-icon"></div>
         <h1>{t.contactSuccessTitle}</h1>
         <p>{t.contactSuccessMessage}</p>
         <Link to="/" className="contact-success-home-btn">
